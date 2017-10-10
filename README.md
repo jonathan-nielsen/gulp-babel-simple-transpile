a853593459985477747eabaaa07388c5108dc240
Jonathan Nielsen <jonathan.nielsen93@gmail.com>
2017-10-10 17:57:41 +0200

    Major release

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # gulp-babel-simple-transpile #
+### Use it like this
+```javascript
+.pipe(babel({
+	sourceMaps: true,
+	presets: [
+		['es2015', { modules: false }],
+		'es2016',
+	],
+}))
+```
--- index.js
+++ index.js
@@ -1,67 +1,50 @@
 'use strict';
 
-import path from 'path';
-import gutil from 'gulp-util';
-import through from 'through2';
+const path = require('path');
+const gutil = require('gulp-util');
+const through = require('through2');
+const babel = require('babel-core');
+const applySourceMap = require('vinyl-sourcemaps-apply');
+const objectAssign = require('object-assign');
 
-import * as babel from 'babel-core';
+module.exports = function(options) {
+	options = options || {
+		presets: ['env']
+	};
 
-// function relPath(base, filePath) {
-// 	filePath = filePath.replace(/\\/g, '/');
-// 	base = base.replace(/\\/g, '/');
-
-// 	if (filePath.indexOf(base) !== 0) {
-// 		return filePath;
-// 	}
-
-// 	const newPath = filePath.slice(base.length);
-
-// 	if (newPath[0] === '/') {
-// 		return newPath.slice(1);
-// 	}
-
-// 	return newPath;
-// }
-
-// function transformFilename(file) {
-// 	// Save the old path for later
-// 	file.revOrigPath = file.path;
-// 	file.revOrigBase = file.base;
-// 	file.revHash = revHash(file.contents);
-
-// 	file.path = modifyFilename(file.path, (filename, extension) => {
-// 		const extIndex = filename.indexOf('.');
-
-// 		filename = extIndex === -1 ?
-// 			revPath(filename, file.revHash) :
-// 			revPath(filename.slice(0, extIndex), file.revHash) + filename.slice(extIndex);
-
-// 		return filename + extension;
-// 	});
-// }
-
-function transpile() {
-	const pathMap = {};
-
-	return through.obj((file, enc, cb) => {
+	return through.obj(function(file, enc, cb) {
 		if (file.isNull()) {
 			cb(null, file);
 			return;
 		}
 
-		console.log(file);
-
 		if (file.isStream()) {
-			cb(new gutil.PluginError('gulp-rev', 'Streaming not supported'));
+			cb(new gutil.PluginError('gulp-esnext', 'Streaming not supported'));
 			return;
 		}
 
-		if (path.extname(file.path) === '.js') {
-			babel.transform()
+		try {
+			const fileOptions = objectAssign({}, options);
+
+			if (file.sourceMap) {
+				fileOptions.sourceFileName = file.relative;
+				fileOptions.sourceMapTarget = file.relative;
+			}
+
+			const str = file.contents.toString();
+			const res = babel.transform(str, fileOptions);
+
+			file.contents = new Buffer(res.code);
+
+			if (res.map && file.sourceMap) {
+				applySourceMap(file, res.map);
+			}
+
+			this.push(file);
+		} catch (err) {
+			this.emit('error', new gutil.PluginError('gulp-esnext', err, { fileName: file.path }));
 		}
 
-		cb(null, file);
+		cb();
 	});
 };
-
-export default transpile;
